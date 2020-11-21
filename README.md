ad456e0007cae627ac417869bb6fe63cb4a6cbbb
Benjamin Congdon <bencongdon96@gmail.com>
2020-11-21 09:09:22 -0800

    Add support for custom URLs and filtering paywalled threads

--- README.md
+++ README.md
@@ -2,6 +2,36 @@
 
 Filter paywalled substack posts from miniflux
 
+## Usage
+
+### CLI Usage
+
+```
+Usage of miniflux-substack-filter:
+  -api-endpoint string
+    	the api of your miniflux instance (default "https://rss.notmyhostna.me")
+  -api-key string
+    	api key used for authentication
+  -dry-run
+    	whether to start in dry run mode
+  -log-level string
+    	the level to filter logs at eg. debug, info, warn, error
+  -password string
+    	the password used to log into miniflux
+  -refresh-interval string
+    	interval defining how often we check for new entries in miniflux
+  -username string
+    	the username used to log into miniflux
+```
+
+### Enabling for a feed
+
+Feeds that have `substack.com` in the URL are automatically filtered for
+paywalled articles. You can also enable filtering on non-substack URLs (e.g.
+substack publications that have a custom domain) by adding `substack_paywall` to
+the Miniflux [rewrite rules](https://miniflux.app/docs/rules.html#rewrite-rules)
+of the feed.
+
 ## Attribution
 
 Inspired by
--- filter/service.go
+++ filter/service.go
@@ -35,21 +35,39 @@ func New(client *miniflux.Client, logger log.Logger, dryRun bool) (*Service, err
 	}, nil
 }
 
+const (
+	paywalledThreadToken = "This thread is only visible to paying subscribers of"
+	rewriteRule          = "substack_paywall"
+)
+
 // RunFilterJob runs the filtering job, which marks paywalled entries as read.
 func (s *Service) RunFilterJob() error {
-	f, err := s.client.Entries(&miniflux.Filter{Status: miniflux.EntryStatusUnread})
+	entries, err := s.client.Entries(&miniflux.Filter{Status: miniflux.EntryStatusUnread})
 	if err != nil {
 		return err
 	}
 
+	feedsResp, err := s.client.Feeds()
+	if err != nil {
+		return err
+	}
+	feeds := make(map[int64]*miniflux.Feed)
+	for _, f := range feedsResp {
+		feeds[f.ID] = f
+	}
+
 	var paywalledEntries []int64
-	for _, entry := range f.Entries {
-		if !strings.Contains(entry.Feed.FeedURL, "substack") {
+	for _, entry := range entries.Entries {
+		feed, ok := feeds[entry.FeedID]
+		feedOptIn := ok && strings.Contains(feed.RewriteRules, rewriteRule)
+		if !feedOptIn && !strings.Contains(entry.Feed.FeedURL, "substack.com") {
 			continue
 		} else if s.cache.Contains(entry.ID) {
 			level.Debug(s.l).Log("msg", "skipping cached entry", "entry_id", entry.ID)
 			continue
 		}
+		level.Debug(s.l).Log("msg", "scraping entry", "url", entry.URL, "entry_id", entry.ID)
+
 		res, err := http.Get(entry.URL)
 		if err != nil || res.StatusCode != 200 {
 			level.Error(s.l).Log("msg", "unable to get entry body", "err", err, "status_code", res.StatusCode)
@@ -62,8 +80,10 @@ func (s *Service) RunFilterJob() error {
 			level.Error(s.l).Log("msg", "unable to parse entry body", "err", err)
 			continue
 		}
-		paywalled := doc.Find("article.post .paywall").Length() > 0
-		level.Debug(s.l).Log("msg", "fetched substack article", "url", entry.URL, "paywalled", paywalled)
+		articlePaywall := doc.Find("article.post .paywall").Length() > 0
+		threadPaywall := strings.Contains(doc.Find(".thread-head").Text(), paywalledThreadToken)
+		paywalled := articlePaywall || threadPaywall
+		level.Debug(s.l).Log("msg", "fetched substack article", "url", entry.URL, "article_paywall", articlePaywall, "thread_paywall", threadPaywall)
 
 		s.cache.Add(entry.ID, true)
 		if !paywalled {
